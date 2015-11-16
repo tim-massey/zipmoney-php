80b905946791ffd0d5008326f98db669da69d2e9
Sagar Bhandari <webgig@Sagars-iMac.local>
2015-11-16 17:15:01 +1100

    WebHook abstract class added

--- lib/WebHook.php
+++ lib/WebHook.php
@@ -8,299 +8,190 @@
  * @link      http://www.zipmoney.com.au/
  */
 
-class ZipMoney_Api
+/*
+ *  Event Types
+-----------------------
+    authorise_succeeded
+    authorise_failed
+    authorise_under_review
+    cancel_succeeded
+    cancel_failed
+    capture_succeeded
+    capture_failed
+    refund_succeeded
+    refund_failed
+    order_cancelled
+    charge_succeeded
+    charge_failed
+*/
+
+abstract class ZipMoney_WebHook
 {
-    protected $_apiConfig = null;
-    
-    protected $_merchantId  = null;
-    
-    protected $_merchantKey = null;
-    
-    protected $_client = null;
-    
-    protected $_params = array();
-    
-    private   $_apiVersion = "1.0.0";
+    private $_merchantId = null;
 
-    private   $_apiHeaders = array();
+    private $_merchantKey = null;
+
+    const EVENT_TYPE_AUTH_SUCCESS = "authorise_succeeded";
+    const EVENT_TYPE_AUTH_FAIL    = "authorise_failed";
+    const EVENT_TYPE_AUTH_REVIEW  = "authorise_under_review";
+
+    const EVENT_TYPE_CANCEL_SUCCESS = "cancel_succeeded";
+    const EVENT_TYPE_CANCEL_FAIL    = "cancel_failed";
+
+    const EVENT_TYPE_CAPTURE_SUCCESS = "capture_succeeded";
+    const EVENT_TYPE_CAPTURE_FAIL    = "capture_failed";
+
+    const EVENT_TYPE_REFUND_SUCCESS = "refund_succeeded";
+    const EVENT_TYPE_REFUND_FAIL    = "refund_failed";
+
+    const EVENT_TYPE_ORDER_CANCELLED = "order_cancelled";
+
+    const EVENT_TYPE_CHARGE_SUCCESS = "charge_succeeded";
+    const EVENT_TYPE_CHARGE_FAIL    = "charge_failed";
     
-    private   $_type = array("json" => "application/json","xml"=>"application/xml");
 
-    const   USER_AGENT  = "ZipMoney PHP SDK";
-
-    const   API_VERSION = "1.0.0";
-
-
-    /**
-     * @param $environment
-     * @param $merchantId
-     * @param $merchantKey
-     * @param $type
-     * @param $config
-     */
-    public function __construct($merchantId, $merchantKey, $environment,  $type = "json", $config = null)
+    public function __construct($merchantId,$merchantKey)
     {
         
- 
-        if(!$merchantId)
-            throw new  ZipMoney_Exception_Http("Merchant Id should be provided", 1);
-
-        if(!$merchantKey)
-            throw new  ZipMoney_Exception_Http("Merchant Key should be provided", 1);
-
-        if(!$environment)
-            throw new  ZipMoney_Exception_Http("Environment should be provided", 1);
-
-        /** @var ZipMoney_ApiSettings $apiSettings */
-        $this->_apiConfig   = new ZipMoney_ApiConfig($environment);
-        $this->_client      = new ZipMoney_Http($this->_apiConfig->getApiBaseUrl(),$type,$config);
-
         $this->_merchantId  = $merchantId;
-        $this->_merchantKey = $merchantKey;   
 
-        $this->_setApiHeaders($type);
-
-        $this->_client->setHttpHeader($this->_apiHeaders);
+        $this->_merchantKey = $merchantKey;
 
     }
 
-    /**
-     * Call checkout method on the endpoint
-     *
-     * @param  $orderArray
-     * @return ZipMoney_Response
-     * @throws ZipMoney_Exception_Http
-     */
-    public function checkout($orderArray)
+    private function _validateCredentials($merchantId,$merchantKey)
     {
-       $method = $this->_apiConfig->getPath(__FUNCTION__);
-       
-       if(!is_array($orderArray))
-            throw new ZipMoney_Exception("Argument should be an array", 1);
 
-    return $this->_request($method, $orderArray);
-    }
-
-    /**
-     * Call cancel method on the endpoint
-     *
-     * @param  $cancelArray
-     * @return ZipMoney_Response
-     * @throws ZipMoney_Exception_Http
-     */
-    public function cancel($cancelArray)
-    {       
-       $method = $this->_apiConfig->getPath("order_cancel");
+        if($merchantId == $this->_merchantId && $merchantKey == $this->_merchantKey){
+            return true;
+        }
+        else{
+            return false;
+        }
         
-        if(!is_array($cancelArray))
-            throw new ZipMoney_Exception("Argument should be an array", 1);
-
-    return $this->_request($method,$cancelArray);
     }
 
-    /**
-     * Call quote method on the endpoint
-     *
-     * @param  $quoteArray
-     * @return ZipMoney_Response
-     * @throws ZipMoney_Exception_Http
-     */
-    public function quote($quoteArray)
-    {        
-        $method = $this->_apiConfig->getPath("quote_quote");
-
-        if(!is_array($quoteArray))
-            throw new ZipMoney_Exception("Argument should be an array", 1);
-
-    return $this->_request($method,$quoteArray);
-    }
-
-    /**
-     * Call refund method on the endpoint
-     *
-     * @param  $refundArray
-     * @return ZipMoney_Response
-     * @throws ZipMoney_Exception_Http
-     */
-    public function refund($refundArray)
+    public function listen()
     {
-        $method = $this->_apiConfig->getPath(__FUNCTION__);
-        
-        if(!is_array($refundArray))
-            throw new ZipMoney_Exception("Argument should be an array", 1);
+        $data = file_get_contents("php://input");
 
-    return $this->_request($method,$refundArray);
+        if (!$data)
+            throw new ZipMoney_Exception("Notification parameters cannot be empty");        
+
+        $params = json_decode($data);
+
+        $this->_processWebHookRequest($params);
     }
 
-    
-    /**
-     * Call query method on the endpoint
-     *
-     * @param  $queryArray
-     * @return ZipMoney_Response
-     * @throws ZipMoney_Exception_Http
-     */
-    public function query($queryArray)
-    {       
-        $method = $this->_apiConfig->getPath(__FUNCTION__);
-        
-        if(!is_array($queryArray))
-            throw new ZipMoney_Exception("Argument should be an array", 1);
 
-    return $this->_request($method,$queryArray);
-    } 
-
-    /**
-     * Call capture method on the endpoint
-     *
-     * @param  $captureArray
-     * @return ZipMoney_Response
-     * @throws ZipMoney_Exception_Http
-     */
-    public function capture($captureArray)
-    {       
-        $method = $this->_apiConfig->getPath(__FUNCTION__);
-        
-        if(!is_array($captureArray))
-            throw new ZipMoney_Exception("Argument should be an array", 1);
-
-    return $this->_request($method,$captureArray);
-    }
-
-    /**
-     * Call settings method on the endpoint
-     *
-     * @param  $queryArray
-     * @return ZipMoney_Response
-     * @throws ZipMoney_Exception_Http
-     */
-    public function settings()
-    {       
-        $method = $this->_apiConfig->getPath("merchant_settings");
-   
-    return $this->_request($method,array());
-    }
-
-    /**
-     * Call configure method on the endpoint
-     *
-     * @return ZipMoney_Response
-     * @throws ZipMoney_Exception_Http
-     */
-    public function configure()
-    {       
-        $method = $this->_apiConfig->getPath("merchant_configure");
-   
-    return $this->_request($method,array());
-    }
-
-    /**
-     * Call heartbeat method on the endpoint
-     *
-     * @param  $captureArray
-     * @return ZipMoney_Response
-     * @throws ZipMoney_Exception_Http
-     */
-    public function heartbeat()
+    protected function _processWebHookRequest($params)
     {
-        $method = $this->_apiConfig->getPath(__FUNCTION__);
-      
-    return  $this->_request($method);
-    }
 
 
-    /**
-     * Get base url
-     *
-     * @param $environment
-     * @return String
-     */
-    public function getBaseUrl($environment = null)
-    {
-        return $this->_apiConfig->getApiBaseUrl($environment);
-    }
+        if ($params->Type == 'SubscriptionConfirmation') {
+            
+            $this->_subscribe($params->SubscribeURL);
 
-    /**
-     * Get endpoint url
-     *
-     * @param $type
-     * @param $environment
-     * @return String
-     */
-    public function getEndpointUrl($endpointType, $environment = null)
-    {
-        return $this->_apiConfig->getUrl($endpointType, $environment);
-    }
+        } else if ($params->Type == 'Notification') {
 
+            if (!$params->Message)
+                throw new ZipMoney_Exception("Notification message cannot be empty");
 
-    /**
-     * Add Http Headers   
-     *  
-     * @param $type
-     */
-    protected function _setApiHeaders($type)
-    {
+            $message  = json_decode($params->Message);
+
+            if (!$message->response)
+                throw new ZipMoney_Exception("Notification response cannot be empty");
+
+            if($this->_validateCredentials($message->response->merchantId,$message->response->merchantKey))
+                throw new ZipMoney_Exception("Merchant Credentials donot match");
+
+            if($message->response->type)
+                $this->_processEvent();
+
+            switch ($message->response->type) {
+                case EVENT_TYPE_AUTH_SUCCESS:
+                    # code...
+                    // $this->eventAuthSuccess();
+                    break;
+                
+                case EVENT_TYPE_AUTH_FAIL:
+                    # code... 
+                    // $this->eventAuthFail();
+                    break;
+                
+                case EVENT_TYPE_AUTH_REVIEW:
+                    # code...
+                    // $this->eventAuthReview();
+                    break;
+                
+                case EVENT_TYPE_CANCEL_SUCCESS:
+                    # code...
+                    // $this->eventCancelSuccess();
+                    break;
+                
+                case EVENT_TYPE_CANCEL_FAIL:
+                    # code...
+                    // $this->eventCancelFail();
+                    break;
+                
+                case EVENT_TYPE_CAPTURE_SUCCESS:
+                    # code...
+                    // $this->eventCaptureSuccess();
+                    break;
+                
+                case EVENT_TYPE_CAPTURE_FAIL:
+                    # code...
+                    // $this->eventCaptureFail();
+                    break;
+                
+                case EVENT_TYPE_REFUND_SUCCESS:
+                    # code...
+                    // $this->eventRefundSuccess();
+                    break;
+                
+                case EVENT_TYPE_REFUND_FAIL:
+                    # code...
+                    // $this->eventRefundFail();
+                    break;
+                
+                case EVENT_TYPE_ORDER_CANCELLED:
+                    # code...
+                    // $this->eventOrderCancel();
+                    break;
+                
+                case EVENT_TYPE_CHARGE_SUCCESS:
+                    # code...
+                    // $this->eventChargeSuccess();
+                    break;
+                
+                case EVENT_TYPE_CHARGE_FAIL:
+                    # code... 
+                    // $this->eventChargeFail();
+                    break;
          
-         $this->_apiHeaders[] = 'Accept: '.$this->_type[$type];
+                default:
+                    # code...
+                    break;
+            }
 
-         $this->_apiHeaders[] = 'Content-Type: '.$this->_type[$type];
-
-         $this->_apiHeaders[] = 'User-Agent: ' . self::USER_AGENT;
-
-         $this->_apiHeaders[] = 'Api-Version: ' . self::API_VERSION;
-
-    }
-
-    /**
-     * 
-     * Add ZipMoney API keys to the request, if not already
-     *
-     */
-    protected  function _addApiKeys()
-    {
-
-        if (!isset($this->_params['merchant_id'])) {
-            $this->_params['merchant_id'] = $this->_merchantId;
-        }
-
-        if (!isset($this->_params['merchant_key'])) { 
-            $this->_params['merchant_key'] = $this->_merchantKey;
         }
 
     }
 
-    /**
-     * Call ZipMoney API endpoint
-     *
-     * @param $method
-     * @param $params
-     * @param int $timeout
-     * @return ZipMoney_Response
-     * @throws ZipMoney_Exception_Http
-     */
-    protected function _request($method, $params = null, $timeout = 60)
-    {
-        $config = array('timeout' => $timeout);
 
-        if(!isset($method) || empty($method))
-            throw new ZipMoney_Exception("Api method not provided", 1);
-        
-        $this->_params = $params;
+
+    private function _subscribe($subscribeUrl)
+    {
+
+        if(!$subscribeUrl)
+            throw new ZipMoney_Exception("SubscribeURL cannot be empty");
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $subscribeUrl;
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($curl);
+        curl_close($curl);
+
+    }
    
-        $this->_addApiKeys();
-
-        // if params is provided, consider it as a POST 
-        if(is_array($params)){      
-
-           $response = $this->_client->post($method,$this->_params);
-        }     
-        else {
-
-           $response = $this->_client->get($method,$this->_params);
-        }
-
-       return new ZipMoney_Response($response);
-    }
-
-    
 }
